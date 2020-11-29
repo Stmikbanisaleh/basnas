571b9eac6b3fd8a4d16f5b8b59427f8554044684
imamsatrianta <imamsatrianta@gmail.com>
2020-11-29 11:22:54 +0700

    update

--- application/controllers/Zakatfitrah.php
+++ application/controllers/Zakatfitrah.php
@@ -144,7 +144,7 @@ class Zakatfitrah extends CI_Controller
 		set_include_path(APPPATH . 'third_party/PHPExcel/Classes/');
 		include 'PHPExcel/IOFactory.php';
 		$objPHPExcel = new PHPExcel();
-		$id_zakatfitrah = $this->model_zakatfitrah->joinzakat()->result_array();
+		$muzakki = $this->model_zakatfitrah->viewOrdering('master_muzakki','id','asc')->result_array();
 		$data = $id_zakatfitrah;
 		$no = 1;
 		$row = 2;
@@ -159,7 +159,6 @@ class Zakatfitrah extends CI_Controller
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('F1', 'Total terima');
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('G1', 'Deskripsi');
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('H1', 'Petugas');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('I1', 'Jenis Zakat');
 
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('A2', '15');
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('B2', 'Tunai');
@@ -169,7 +168,6 @@ class Zakatfitrah extends CI_Controller
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('F2', '1500000');
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('G2', 'import');
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('H2', '123456');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('I2', '');
 
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('A3', '17');
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('B3', 'Tunai');
@@ -179,97 +177,25 @@ class Zakatfitrah extends CI_Controller
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('F3', '1500000');
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('G3', 'import');
 				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('H3', '123456');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('I3', '');
 
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('K1', 'Id Muzakki');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('L1', 'Nama Muzakki');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('M1', 'Cara Terima');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('M2', 'Transfer');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('M3', 'Tunai');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('N1', 'Tgl Terima');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('O1', 'ID NOREK');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('P1', 'NOREK');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('Q1', 'BANK');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('R1', 'Jenis');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('S1', 'Total Terima');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('T1', 'Deskripsi');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('U1', 'Petugas');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('V1', 'Jenis Zakat');
-				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('W1', 'Nama Jenis Zakat');
-				foreach ($data as $dataExcel) {
-					$id_muzakki = $dataExcel['id_muzakki'];
-					$nama = $dataExcel['nama'];
-					$tgl_terima = $dataExcel['tgl_terima'];
-					$rek = $dataExcel['rek'];
-					$nokartu = $dataExcel['nokartu'];
-					$namabank = $dataExcel['namabank'];
-					$jenis = $dataExcel['jenis'];
-					$total_terima = $dataExcel['total_terima'];
-					$deskripsi = $dataExcel['deskripsi'];
-					$petugas = $dataExcel['petugas'];
-					$jenis_zakat = $dataExcel['jenis_zakat'];
-					$nama_jenis_zakat = $dataExcel['nama_jenis_zakat'];
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('K1', 'Id Jenis');
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('L1', 'Nama Jenis');
+		
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('K2', '1');
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('L2', 'Zakat Fitrah');
+			
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('K3', '2');
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('L3', 'Zakat Mal');
 
-					$objPHPExcel->getActiveSheet(0)->getStyle('K' . $row)->getNumberFormat()->setFormatCode('yyyy-mm-dd');
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('K' . $row, $id_muzakki, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('K')->setAutoSize(true);
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('K4', '3');
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue('L4', 'Infaq');
 
-					$objPHPExcel->getActiveSheet(0)->getStyle('L' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('L' . $row, $nama, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('L')->setAutoSize(true);
-
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('N' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_NUMBER);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('N' . $row, $tgl_terima, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('N')->setAutoSize(true);
-
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('O' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_NUMBER);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('O' . $row, $rek, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('O')->setAutoSize(true);
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('P' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('P' . $row, $nokartu, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('P')->setAutoSize(true);
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('Q' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('Q' . $row, $namabank, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('Q')->setAutoSize(true);
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('R' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('R' . $row, $jenis, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('R')->setAutoSize(true);
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('S' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_NUMBER);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('S' . $row, $total_terima, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('S')->setAutoSize(true);
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('T' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('T' . $row, $deskripsi, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('T')->setAutoSize(true);
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('U' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('U' . $row, $petugas, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('U')->setAutoSize(true);
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('V' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('V' . $row, $jenis_zakat, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('V')->setAutoSize(true);
-
-					$objPHPExcel->getActiveSheet(0)->getStyle('W' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
-					$objPHPExcel->getActiveSheet(0)->setCellValueExplicit('W' . $row, $nama_jenis_zakat, PHPExcel_Cell_DataType::TYPE_STRING);
-					$objPHPExcel->getActiveSheet(0)->getColumnDimension('W')->setAutoSize(true);
-
-					$row++;
-					$no++;
-				}
 				header('Content-Type: application/vnd.ms-excel; charset=utf-8');
-				header('Content-Disposition: attachment; filename=report.xls');
+				header('Content-Disposition: attachment; filename=Format_upload_transaksi_zakat_fitrah.xls');
 				header('Cache-Control: max-age=0');
 				ob_end_clean();
 				ob_start();
 				$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-				$filename = 'Sample' . 'csv';
 				$objWriter->save('php://output');
 			}
 		}
@@ -283,9 +209,7 @@ class Zakatfitrah extends CI_Controller
 			$file = $_FILES['file']['name'];
 			$fname = $_FILES['file']['tmp_name'];
 			$ext = explode('.', $file);
-			/** Include path **/
 			set_include_path(APPPATH . 'third_party/PHPExcel/Classes/');
-			/** PHPExcel_IOFactory */
 			include 'PHPExcel/IOFactory.php';
 			$objPHPExcel = PHPExcel_IOFactory::load($fname);
 			$allDataInSheet = $objPHPExcel->getActiveSheet()->toArray(null, false, true);
@@ -304,10 +228,6 @@ class Zakatfitrah extends CI_Controller
 					if (!$value[0]) {
 						array_push($empty_message, "No at row "  . $keys . " Id Muzakki Regitrasi harus di isi");
 					}
-
-
-
-
 					if (!empty($empty_message)) {
 						$ret['msg'] = $empty_message;
 						$this->session->set_flashdata('message', '' . json_encode($ret['msg']));
@@ -346,36 +266,5 @@ class Zakatfitrah extends CI_Controller
 			'filename'	=> $filename
 		);
 		$this->load->view('page/zakatfitrah/transaksi_zakatfitrah', $data);
-
-		// $this->load->library('pdf');
-
-		// $this->pdf->setPaper('FOLIO', 'potrait');
-		// $this->pdf->filename = "Tanda-Terima-Zakatfitrah". "-" .$data['my_data'][0]->nama . "-" . date('Y-m-d') . ".pdf";
-		// $this->pdf->load_view('page/zakatfitrah/transaksi_zakatfitrah', $data);
 	}
-	// public function laporan_pdf(){
-	//     $tgl = $this->mainfunction->tgl_indo(date('Y-m-d'));
-	//     $nis = $this->input->post('siswa');
-	//     $kelas = $this->input->post('kelas');  
-	//     $my_pembsiswa = $this->model_surattagihan->view_siswatg($nis, $kelas)->row();
-	//     $thnakad = $this->configfunction->getthnakd();
-	//     $this->load->library('pdf');
-
-	//     $setting = $this->model_surattagihan->view('sys_config','id','asc')->row();
-	//     if($my_pembsiswa!=null){
-	//         $data = array(
-	//             'mydata'      => $my_pembsiswa,
-	//             'tgl'         => $tgl,
-	//             'setting'     => $setting,
-	//             'thnakad'     => $thnakad[0]['THNAKAD']
-
-	//         );
-	//         $this->pdf->setPaper('FOLIO', 'potrait');
-	//         $this->pdf->filename = "Surat-Tagihan".$nis."-".date('Y-m-d').".pdf";
-	//         $this->pdf->load_view('pagekasir/surattagihan/laporan', $data);
-	//     }else{
-	//         $this->session->set_flashdata('cat_success', 'Data Tidak Ditemukan!');
-	//         header("Location: ".base_url()."modulkasir/surattagihan");
-	//     }
-	// }
 }
